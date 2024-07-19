937634289a27c19de25785b0ce85c59dff6ce803
Sunat Nur <uzbekistan9699@gmail.com>
2024-07-19 14:28:31 +0900

    feat:react Native  Navigation/ stack

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import AppNavigation from '../navigations/app.navigation';
+
+export default function App() {
+    return <AppNavigation />
+};
---
+++ navigations/app.navigation.js
@@ -0,0 +1,25 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import Home from "../screens/home";
+import Detailed from "../screens/detailed";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+    return (
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={Detailed}
+                    options={{headerShown: false}}
+                />
+            </Stack.Navigator>
+    );
+}
+
