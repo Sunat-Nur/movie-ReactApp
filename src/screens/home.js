0cd10a6baf9b79840153b63e28784337ba60e554
Sunat Nur <uzbekistan9699@gmail.com>
2024-07-22 13:37:20 +0900

    feat: create showsVerticalScrollIndicator and SrollView

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StatusBar} from 'expo-status-bar';
 import {MagnifyingGlassCircleIcon} from "react-native-heroicons/outline";
@@ -9,11 +9,16 @@ export default function Home({navigation}) {
         <View className="flex-1 bg-slate-950">
             <SafeAreaView>
                 <StatusBar style="light"/>
-                <View className="p-4 flex-row items-center">
+                <View className="flex-row justify-between items-center mx-4  border-b-2">
                     <Image source={require('../../assets/images/logo.png')}/>
                     <MagnifyingGlassCircleIcon/>
                 </View>
             </SafeAreaView>
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 20}}
+            ></ScrollView>
         </View>
     );
 }
